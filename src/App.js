cdc3201c11d7073f27fe598452fd4f2f8ea23c2a
DarkAsirius <kirill.samokhin.05@mail.ru>
2025-01-07 02:52:18 +0300

    Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import Goods from './components/blocks/Goods';
 import Gallery from './components/blocks/Gallery/';
 import YandexMap from './components/blocks/YandexMap';
 import Footer from './components/blocks/Footer';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCccZbbK5z7yGk05eiZnWAGs5PMKM9t-G4",
+  authDomain: "na-zare.firebaseapp.com",
+  projectId: "na-zare",
+  storageBucket: "na-zare.firebasestorage.app",
+  messagingSenderId: "762125239584",
+  appId: "1:762125239584:web:32a7e1e12c75d446be8345",
+  measurementId: "G-FPNXFS1XL8"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   return (
