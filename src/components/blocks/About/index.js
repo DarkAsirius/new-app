cf026c4057f4204aece8204eb167c98854e0385d
DarkAsirius <kirill.samokhin.05@mail.ru>
2025-01-12 06:24:11 +0300

    Finaly

--- src/components/blocks/About/index.js
+++ src/components/blocks/About/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import styles from "../About/About.module.css";
-import Container from "../../UI/Container"
-
 const About = () =>{
     return(
         <div className={styles.about} id="about">
             <img src="../../../photo1.jpg" className={styles.image} alt="img-1"></img>
-            <Container>
                 <div className={styles.overlay}>
                     <img src="../../../logo_white.png" className={styles.logo} alt="Logo" />
                     <div className={styles.text}>
@@ -17,7 +14,6 @@ const About = () =>{
                         <p className={styles.desc}>"На Заре" – это место, где каждый гость может насладиться атмосферой уюта и спокойствия, погрузившись в мир чая. Мы предлагаем широкий ассортимент сортов чая со всего мира, от классических черных и зеленых до редких травяных и фруктовых смесей.</p>
                     </div>
                 </div>
-            </Container>
         </div>
     )
 }
