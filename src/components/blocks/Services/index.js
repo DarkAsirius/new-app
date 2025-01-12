fac3fa826077f468b024426691cd59777610221e
DarkAsirius <kirill.samokhin.05@mail.ru>
2025-01-12 06:10:03 +0300

    Finaly

--- src/components/blocks/Services/index.js
+++ src/components/blocks/Services/index.js
@@ -5,22 +5,22 @@ const servicesData = [
     {
         title: 'Чай в стакане',
         description: 'Можете заварить чай и взять его с собой',
-        icon: <img src="../../../ServicesIcons/чай стакан.png"></img>
+        icon: <img src="../../../ServicesIcons/чай стакан.png" alt="cup"></img>
     },
     {
         title: 'Чай с собой',
         description: 'Можете взять чай на вынос и распить его дома',
-        icon: <img src="../../../ServicesIcons/чайнавынос.png"></img>
+        icon: <img src="../../../ServicesIcons/чайнавынос.png" alt="home"></img>
     },
     {
         title: 'Пин Ча',
         description: 'Распитие чая проливами с нашей посудой',
-        icon: <img src="../../../ServicesIcons/пинча.png"></img>
+        icon: <img src="../../../ServicesIcons/пинча.png" alt="pincha"></img>
     },
     {
         title: 'Церемония',
         description: 'Проведение частных и выездных чайных церемоний',
-        icon: <img src="../../../ServicesIcons/черемонии.png"></img>
+        icon: <img src="../../../ServicesIcons/черемонии.png" alt="ceremony"></img>
     }
 ];
 
