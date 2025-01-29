42ea9fd7c34555917d75552bf2266ad3ad52bc09
DarkAsirius <kirill.samokhin.05@mail.ru>
2025-01-30 00:17:25 +0300

    Final

--- src/components/blocks/About/index.js
+++ src/components/blocks/About/index.js
@@ -3,7 +3,15 @@ import styles from "../About/About.module.css";
 const About = () =>{
     return(
         <div className={styles.about} id="about">
-            <img src="../../../титул.jpg" className={styles.image} alt="img-1"></img>  
+            <img src="../../../титул фон.jpg" className={styles.image} alt="img-1"></img>
+                <div className={styles.overlay}>
+                    <img src="../../../logo_white.png" className={styles.logo} alt="Logo" />
+                    <div className={styles.text}>
+                        <div className={styles.deviz}>
+                            <h1 className={styles.head}>ОЗАРИМ ВАШ ЧАЙНЫЙ ПУТЬ</h1>
+                        </div>
+                    </div>
+                </div>
         </div>
     )
 }
