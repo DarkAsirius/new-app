db25a9dd672a4a6cd4c10de76c8478058021d42d
DarkAsirius <kirill.samokhin.05@mail.ru>
2025-02-25 22:59:16 +0300

    final

--- src/components/blocks/YandexMap/index.js
+++ src/components/blocks/YandexMap/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect} from 'react';
 import './YandexMap.css'; // Подключаем ваши стили
 import Contacts from '../Contacts';
 
